193e60657b1f97354d7cdaa61d02ed351deffb6d
hamidaslami <hamidrezaaslami1379@gmail.com>
2025-05-05 15:36:03 +0330

    update ffmpg

--- lib/src/process.dart
+++ lib/src/process.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 
-import 'package:ffmpeg_kit_flutter_minimal/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_flutter_minimal/ffmpeg_session.dart';
-import 'package:ffmpeg_kit_flutter_minimal/log.dart';
-import 'package:ffmpeg_kit_flutter_minimal/statistics.dart';
 
+
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_new/ffmpeg_session.dart';
+import 'package:ffmpeg_kit_flutter_new/log.dart';
+import 'package:ffmpeg_kit_flutter_new/statistics.dart';
 import 'package:render/src/formats/abstract.dart';
 import 'package:render/src/service/notifier.dart';
 import 'package:render/src/service/session.dart';
